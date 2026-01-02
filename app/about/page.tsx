555a60bfec43ca5f2fdffd9f5cc40aa0f5c5404a
Chukwuebuka Michael Umeh <chukwuebuka.umeh@selu.edu>
2026-01-02 17:46:09 -0600

    About section and about page completed

---
+++ app/about/page.tsx
@@ -0,0 +1,21 @@
+const AboutPage = () => {
+  return (
+    <div className="max-w-4xl mx-auto py-16 px-6">
+      <div className="flex flex-col items-center lg:items-start mb-4">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl font-extrabold">
+          About EWA
+        </h2>
+        <div className="w-68 h-1 bg-gradient-to-r from-wine-primary to-cocoa-primary rounded-full mt-2"></div>
+      </div>
+      <p className="text-lg text-gray-300 mb-4">
+        EWA began with a deeper realization about hair care, not just a product. When I shared my personal hair care routine and guidelines, I initially encouraged people to use any oil or leave-in conditioner of their choice. At the time, I believed that consistency mattered more than the specific products being used.
+        That belief changed after I had an experience with a leave-in conditioner that did absolutely nothing for my hair. It sat on my strands without penetrating, without nourishing, and without delivering the results it promised. It made me pause and think, what if this is what others are using too? How can healthy hair be achieved if the products meant to support it aren’t actually doing the work?
+        I returned to the products I knew and trusted, and the difference was immediate. That moment made it clear to me that not all oils and leave-in conditioners are created equally, and that recommending “any product” isn’t always helpful if it doesn’t truly serve the hair.
+        Around that same time, people who had received my routine began reaching out, asking what oil I personally use. When I shared that I use my own homemade herbal oil, the response surprised me, people didn’t just want the routine, they wanted the oil itself. Within days, multiple people asked to purchase it, and that’s how EWA moved from an idea into action.
+        EWA was created to offer hair care that is intentional, effective, and trustworthy, starting with scalp health. What began as something I made for myself has now become something I’m sharing with others. Today, I’m fulfilling my first orders, building with purpose, and growing EWA step by step; rooted in care, experience, and a commitment to beauty for your crown.
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import PageHeader from "./components/PageHeader";
 import IntroSection from "./components/IntroSection";
 import { ProductsPreview } from "./components/ProductsPreview";
+import AboutSection from "./components/AboutSection";
 export default function Home() {
   return (
     <div className="font-sans">
       <IntroSection></IntroSection>
+      <AboutSection></AboutSection>
       <ProductsPreview></ProductsPreview>
+
     </div>
   );
 }
